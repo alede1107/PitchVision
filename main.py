3ef0c07697557dffc6ca3b6d76ac769d8f36ce67
alede1107 <alejandro.perdomo823@gmail.com>
2026-06-24 15:26:51 -0400

    foundation of cli tool

--- main.py
+++ main.py
@@ -11,21 +11,38 @@ api_key = os.environ["GENAI_KEY"]
 
 client = genai.Client(api_key=api_key)
 
+'''
 interaction = client.models.generate_content(
     model="gemini-2.5-flash",
     contents="Explain how AI works in a few words"
 )
-print(interaction.text)
+'''
+#print(interaction.text)
 
 ## Setting up World Cup API
 
 wc_token = os.environ["WC_TOKEN"]
 headers = {"Authorization": f"Bearer {wc_token}"}
 
-response = requests.get("https://worldcup26.ir/get/teams", headers=headers)
+response = requests.get("https://worldcup26.ir/get/games", headers=headers)
 
-print(response.json())
-
-## Set up SQL database -- Stores queries from user
+games = response.json()["games"]
 
 ## Ask user for team name -> checks if team is playing -> returns Gemini-powered output
+
+team = input("What team are you looking for? ").lower().strip()
+
+print(team)
+
+
+# Fix logic. Need to find a way to not print out "Not Playing" repeatedly
+
+for game in games:
+  home = game.get("home_team_name_en", "").lower()
+  away = game.get("away_team_name_en", "").lower()
+
+  if team == home or team == away:
+    if game.get("time_elapsed") == "live":
+      print("Playing!") # Stub
+    else:
+      print("Not playing currently") 
